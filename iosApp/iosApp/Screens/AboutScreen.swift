3b38ce87d6e902562c2b2fce08102451952885fc
FallenDemon2000 <amendona6@gmail.com>
2025-12-27 15:14:17 +0000

    section 4: add ui with jetpack compose and swift ui

---
+++ iosApp/iosApp/Screens/AboutScreen.swift
@@ -0,0 +1,36 @@
+//
+//  AboutScreen.swift
+//  iosApp
+//
+//  Created by Ângelo Mendonça on 20/12/2025.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct AboutScreen:View {
+    
+    @Environment(\.dismiss)
+    private var dismiss
+    
+    var body: some View {
+        NavigationStack {
+            AboutListView()
+                .navigationTitle ("About Device")
+                .toolbar {
+                    ToolbarItem(placement: .primaryAction) {
+                        Button {
+                            dismiss()
+                        } label: {
+                            Text("Done")
+                                .bold()
+                        }
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    AboutScreen()
+}
